b5c3d4e44ab243deb319757e3b022081ae0178f4
Samir Aleido <eidosam@hotmail.com>
2018-08-24 22:44:32 +0200

    :tada: init: Create React App with Material-UI design

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = (theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+});
+
+class Header extends Component {
+    render () {
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" className={classes.flex}></Typography>
+                        <Avatar alt="Hazzaa" src="/assets/hazzaa.png" className={classes.avatar} />
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
