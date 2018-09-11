db11e6626492ae666eb10a43f991ced547cf44f3
Samir Aleido <eidosam@hotmail.com>
2018-09-11 16:07:02 +0200

    :fire: refactor: Remove custom theme

--- src/withRoot.js
+++ src/withRoot.js
@@ -1,24 +1,9 @@
 
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import { CssBaseline } from '@material-ui/core';
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            light: purple[300],
-            main: purple[500],
-            dark: purple[700],
-        },
-        secondary: {
-            light: green[300],
-            main: green[500],
-            dark: green[700],
-        },
-    },
-});
+const theme = createMuiTheme();
 
 function withRoot (Component) {
     function WithRoot (props) {
